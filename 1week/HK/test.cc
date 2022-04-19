131bf0bae9f7e5e9f6b75016420d39f01dd02991
hekang42 <hekang@student.42seoul.kr>
2022-04-19 11:34:58 +0900

    Set testing module

--- 1week/HK/test.cc
+++ 1week/HK/test.cc
@@ -1,23 +1,12 @@
-#include <gtest/gtest.h>
+#include "../../gtest/include/gtest/gtest.h"
 #include "arraylist.h"
 #include <stdio.h>
-
 namespace {
-TEST(create, negative_int) { EXPECT_EQ(createArrayList(-1), nullptr); };
-
-TEST(create, positive_int) { EXPECT_NE(createArrayList(5), nullptr); };
-
-TEST(create, maximum_int) { EXPECT_NE(createArrayList(2147483647), nullptr); };
-
-TEST(delete, deleteArray) {
-    ArrayList *arr = createArrayList(10);
-    // deleteArrayList(arr);
-    deleteArrayList(arr);
-
-    EXPECT_DEATH(arr->currentElementCount, "DEATH");
-    // double free를 잡자.
+TEST(arrayList, case1){
+    // EXPECT_EQ(2, sum(1, 1));
+    // EXPECT_EQ(3, sum(1, 1));
 };
-
+TEST(arrayList, case2) {}
 } // namespace
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
