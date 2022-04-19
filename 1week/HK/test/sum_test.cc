64fd8d18f89da116fbd672bbc48b87a6b4dd87b6
hekang42 <hekang@student.42seoul.kr>
2022-04-19 10:42:51 +0900

    1week HK

---
+++ 1week/HK/test/sum_test.cc
@@ -0,0 +1,14 @@
+#include "../../gtest/gtest.h"
+#include <stdio.h>
+
+#include "testExample.h"
+
+TEST(test_case_name, test_name) {
+    EXPECT_EQ(2, sum(1, 1));
+    EXPECT_EQ(3, sum(1, 1));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ 1week/HK/test/testExample.cc
@@ -0,0 +1,3 @@
+#include "testExample.h"
+
+int sum(int a, int b) { return a + b; }
