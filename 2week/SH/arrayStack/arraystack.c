f2acff172cbc7a177b51f53b8e4c788a1c50c1cb
hekang42 <hekang@student.42seoul.kr>
2022-04-28 09:59:41 +0900

    modify create push delete Stack

--- 2week/SH/arrayStack/arraystack.c
+++ 2week/SH/arrayStack/arraystack.c
@@ -1,9 +1,43 @@
 #include "arraystack.h"
+#include <stdlib.h>
 
-ArrayStack *createArrayStack(int maxElementCount);
-int pushLS(ArrayStack *pStack, StackNode element);
+ArrayStack *createArrayStack(int maxElementCount) {
+    ArrayStack *stack;
+
+    if (maxElementCount <= 0)
+        return NULL;
+    stack = (ArrayStack *)calloc(1, sizeof(ArrayStack));
+    if (!stack)
+        return NULL;
+    stack->pElement = (StackNode *)calloc(maxElementCount, sizeof(StackNode));
+    if (!stack->pElement)
+        return NULL;
+    stack->maxElementCount = maxElementCount;
+    return stack;
+};
+
+int pushLS(ArrayStack *pStack, StackNode element) {
+    if (!pStack)
+        return -1;
+    if (pStack->currentElementCount < pStack->maxElementCount) {
+        pStack->pElement[pStack->currentElementCount] = element;
+        ++pStack->currentElementCount;
+        return TRUE;
+    } else
+        return FALSE;
+}
+
 StackNode *popLS(ArrayStack *pStack);
 StackNode *peekLS(ArrayStack *pStack);
-void deleteArrayStack(ArrayStack *pStack);
+void deleteArrayStack(ArrayStack *pStack) {
+    if (!pStack)
+        return;
+    if (pStack->pElement) {
+        free(pStack->pElement);
+        pStack->pElement = NULL;
+    }
+    free(pStack);
+    return;
+}
 int isArrayStackFull(ArrayStack *pStack);
 int isArrayStackEmpty(ArrayStack *pStack);
--- 2week/SH/arrayStack/test.cc
+++ 2week/SH/arrayStack/test.cc
@@ -1,10 +1,58 @@
 #include "../../../gtest/include/gtest/gtest.h"
 #include "arraystack.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // testCaseName When_StateUnderTest_Expect_ExpectedBehavior
 
-TEST(create, must_success) { EXPECT_NE(createArrayStack(), nullptr); };
+ArrayStack *stack;
+
+TEST(create, when_create_expect_notNull) {
+    stack = createArrayStack(1);
+    EXPECT_NE(stack, nullptr);
+    free(stack);
+};
+
+TEST(create, when_createMinus1_expect_Null) {
+    EXPECT_EQ(createArrayStack(0), nullptr);
+    EXPECT_EQ(createArrayStack(-1), nullptr);
+};
+
+TEST(create, when_createMaxelement1_expect_1) {
+    stack = createArrayStack(1);
+    EXPECT_EQ(stack->maxElementCount, 1);
+    free(stack);
+};
+
+TEST(pushLS, when_NullStack_expect_minus1) {
+    StackNode node;
+    stack = nullptr;
+    // stack = createArrayStack(1);
+    node.data = 0;
+
+    EXPECT_EQ(pushLS(stack, node), -1);
+}
+
+TEST(pushLS, when_StackNotMaxElement_expect_1) {
+    StackNode node;
+    stack = createArrayStack(1);
+    node.data = 0;
+
+    EXPECT_EQ(pushLS(stack, node), 1);
+    free(stack);
+}
+
+TEST(pushLS, when_StackMaxElement_expect_1) {
+    StackNode node;
+    stack = createArrayStack(1);
+    node.data = 0;
+    pushLS(stack, node);
+
+    EXPECT_EQ(pushLS(stack, node), 0);
+    free(stack);
+}
+
+TEST(leaksTest, logs) { system("leaks test > logs"); }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
