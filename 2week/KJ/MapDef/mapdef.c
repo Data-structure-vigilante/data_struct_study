d608bf3c6db0f97fc5bd64c1c71167bdf4e8ea09
nong508 <86358498+nong508@users.noreply.github.com>
2022-05-03 12:27:42 +0900

    save files

--- 2week/KJ/MapDef/mapdef.c
+++ 2week/KJ/MapDef/mapdef.c
@@ -69,31 +69,3 @@ void printMaze(int mazeArray[HEIGHT][WIDTH])
 		++height;
 	}
 }
-
-LinkedStack *searchPath(int map[][], MapPosition startPos)
-{
-	LinkedStack *result;
-
-	//도착 지점을 찾았거나, 모든 경로가 막혔을 경우.
-	while (1)
-	{
-		break;
-		// 현재 보고있는 좌표가 벽이다.
-		// -> 다른 방향을 본다.
-
-		// 현재 보고있는 좌표가 방문한 곳이다.
-		// -> 다른 방향을 본다.
-
-		// 현재 보고있는 좌표가 방문하지 않은 곳이다.
-		// -> 간다
-
-		// 사 방에 대해서 확인을 다 했는데도 위 조건에 맞게 처리되지 않았을
-		// 경우에만 여기로 네 방향으 모두 벽이거나 방문했다면 빠구할 때는 pop
-		// 타겟을 벽으로 변경
-
-		//탈출 조건
-		// 1. 현재 타겟 포지션이 Goal이다.
-		// 2. 스택이 엠티다.
-	}
-	return NULL;
-}
