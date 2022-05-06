ad62c53594692c4f6ce4fe1b184a7c9ab6bad879
Likilee <gnsdlrl@daum.net>
2022-05-06 10:46:27 +0900

    Update queue

--- 3week/SK/ArrayQueue/arrayqueue.c
+++ 3week/SK/ArrayQueue/arrayqueue.c
@@ -1,54 +1,73 @@
 #include "arrayqueue.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-ArrayQueue* createArrayQueue(int maxElementCount) {
-	ArrayQueue*	queue;
+ArrayQueue *createArrayQueue(int maxElementCount) {
+    ArrayQueue *queue;
 
-	if (maxElementCount <= 0) {
-		fprintf(stderr, "Invalid input");
-		return NULL;
-	}
-	queue = (ArrayQueue*)calloc(1, sizeof(ArrayQueue));
-	if (queue == NULL)
-		fprintf(stderr, "memory allocat fail");
-	queue->pElement = (ArrayQueueNode*)calloc(maxElementCount, sizeof(ArrayQueueNode));
-	if (queue->pElement == NULL)
-		fprintf(stderr, "memory allocat fail");
-	queue->maxElementCount = maxElementCount;
-	queue->front = -1;
-	queue->rear = -1;
-	return queue;
+    if (maxElementCount <= 0) {
+        fprintf(stderr, "Invalid input");
+        return NULL;
+    }
+    queue = (ArrayQueue *)calloc(1, sizeof(ArrayQueue));
+    if (queue == NULL)
+        fprintf(stderr, "memory allocat fail");
+    queue->pElement =
+        (ArrayQueueNode *)calloc(maxElementCount, sizeof(ArrayQueueNode));
+    if (queue->pElement == NULL)
+        fprintf(stderr, "memory allocat fail");
+    queue->maxElementCount = maxElementCount;
+    return queue;
 }
 
-int enqueueAQ(ArrayQueue* pQueue, ArrayQueueNode element) {
-	int idx = 0;
+int enqueueAQ(ArrayQueue *pQueue, ArrayQueueNode element) {
+    int idx = 0;
 
-	if (pQueue == NULL)
-		return ERROR;
-	
-	if (pQueue->currentElementCount == pQueue->maxElementCount) {
-		fprintf(stderr, "Queue is full");
-		return FALSE;
-	}
+    if (pQueue == NULL)
+        return ERROR;
 
-	if (pQueue->currentElementCount == 0)
-		++pQueue->front;
-
-	idx = ++pQueue->rear;
-	pQueue->pElement[idx] = element;
-	++pQueue->currentElementCount;
-	pQueue->rear = pQueue->rear % pQueue->maxElementCount;
-	return TRUE;
+    if (pQueue->currentElementCount == pQueue->maxElementCount) {
+        fprintf(stderr, "Queue is full");
+        return FALSE;
+    }
+    //비었을 때 다르게 동작해야하는게 있나>? (빈 큐일 때, 프론트와 리어가
+    //가르키고 있는 인덱스에 요소가 없다.)
+    if (pQueue->currentElementCount != 0) {
+        ++pQueue->rear;
+        pQueue->rear = pQueue->rear % pQueue->maxElementCount;
+    }
+    idx = pQueue->rear;
+    pQueue->pElement[idx] = element;
+    ++pQueue->currentElementCount;
+    return TRUE;
 }
 
-ArrayQueueNode *dequeueAQ(ArrayQueue* pQueue) {
+ArrayQueueNode *dequeueAQ(ArrayQueue *pQueue) {
+    int idx = 0;
 
+    if (pQueue == NULL || pQueue->currentElementCount == 0)
+        return NULL;
+    --pQueue->currentElementCount;
+
+		idx = pQueue->front;
+
+		if (pQueue->currentElementCount != 0) {
+        ++pQueue->front;
+        pQueue->rear = pQueue->rear % pQueue->maxElementCount;
+    }
+
+		return &pQueue->pElement[idx];
 }
 
-ArrayQueueNode *peekAQ(ArrayQueue* pQueue);
-void deleteArrayQueue(ArrayQueue* pQueue) {
+ArrayQueueNode *peekAQ(ArrayQueue *pQueue);
 
+void deleteArrayQueue(ArrayQueue *pQueue) {
+    if (pQueue == NULL)
+        return;
+    free(pQueue->pElement);
+    pQueue->pElement = NULL;
+    free(pQueue);
 }
-int isArrayQueueFull(ArrayQueue* pQueue);
-int isArrayQueueEmpty(ArrayQueue* pQueue);
+
+int isArrayQueueFull(ArrayQueue *pQueue);
+int isArrayQueueEmpty(ArrayQueue *pQueue);
--- 3week/SK/ArrayQueue/test.cc
+++ 3week/SK/ArrayQueue/test.cc
@@ -1,52 +1,77 @@
+#include "arrayqueue.h"
 #include <gtest/gtest.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arrayqueue.h"
 
 TEST(create, when_createWithZero_expect_NULL) {
-	EXPECT_EQ(createArrayQueue(0), nullptr);
+    EXPECT_EQ(createArrayQueue(0), nullptr);
 };
 
 TEST(create, when_createWithNegative_expect_NULL) {
-	EXPECT_EQ(createArrayQueue(-1), nullptr);
+    EXPECT_EQ(createArrayQueue(-1), nullptr);
 };
 
 TEST(create, when_createWithValidInput_expect_NotNULL) {
-	ArrayQueue *queue;
+    ArrayQueue *queue;
 
-	queue = createArrayQueue(10);
-	EXPECT_NE(queue, nullptr);
-	EXPECT_EQ(queue->maxElementCount, 10);
-	EXPECT_EQ(queue->front, -1);
-	EXPECT_EQ(queue->rear, -1);
-	deleteArrayQueue(queue);
+    queue = createArrayQueue(10);
+    EXPECT_NE(queue, nullptr);
+    EXPECT_EQ(queue->maxElementCount, 10);
+    EXPECT_EQ(queue->front, 0);
+    EXPECT_EQ(queue->rear, 0);
+    deleteArrayQueue(queue);
 }
 
 TEST(enqueue, when_enqueueToEmptyQueue_expect_TRUE) {
-	ArrayQueue *queue;
-	ArrayQueueNode	temp;
+    ArrayQueue *queue;
+    ArrayQueueNode temp;
 
-	temp.data = 1;
-	queue = createArrayQueue(10);
-	enqueueAQ(queue, temp);
-	EXPECT_EQ(queue->front, 0);
-	EXPECT_EQ(queue->rear, 0);
-	EXPECT_EQ(queue->currentElementCount, 1);
-	deleteArrayQueue(queue);
+    temp.data = 1;
+    queue = createArrayQueue(10);
+    enqueueAQ(queue, temp);
+    EXPECT_EQ(queue->front, 0);
+    EXPECT_EQ(queue->rear, 0);
+    EXPECT_EQ(queue->currentElementCount, 1);
+    deleteArrayQueue(queue);
 }
 
 TEST(enqueue, when_enqueueToEmptyQueue_expect_FALSE) {
-	ArrayQueue *queue;
-	ArrayQueueNode	temp;
+    ArrayQueue *queue;
+    ArrayQueueNode temp;
 
-	temp.data = 1;
-	queue = createArrayQueue(1);
-	enqueueAQ(queue, temp);
-	EXPECT_EQ(queue->front, 0);
-	EXPECT_EQ(queue->rear, 0);
-	EXPECT_EQ(queue->currentElementCount, 1);
-	deleteArrayQueue(queue);
+    temp.data = 1;
+    queue = createArrayQueue(1);
+    enqueueAQ(queue, temp);
+    EXPECT_EQ(queue->front, 0);
+    EXPECT_EQ(queue->rear, 0);
+    EXPECT_EQ(queue->currentElementCount, 1);
+    deleteArrayQueue(queue);
+}
+
+TEST(dequeueAQ, when_deQueToEmptyQueue_expect_NULL) {
+    ArrayQueue *queue;
+    queue = createArrayQueue(1);
+
+    EXPECT_EQ(dequeueAQ(queue), nullptr);
+    deleteArrayQueue(queue);
+}
+
+TEST(dequeueAQ, when_1ElementQueue_expect_element) {
+    ArrayQueue *queue;
+    ArrayQueueNode temp;
+		ArrayQueueNode *result;
+
+    temp.data = 1;
+    queue = createArrayQueue(1);
+    enqueueAQ(queue, temp);
+		result = dequeueAQ(queue);
+		EXPECT_NE(result, nullptr);
+		EXPECT_EQ(result->data, 1);
+		EXPECT_EQ(queue->currentElementCount, 0);
+		EXPECT_EQ(queue->front, 0);
+		EXPECT_EQ(queue->rear, 0);
+    deleteArrayQueue(queue);
 }
 
 int main(int argc, char **argv) {
