71972687a2fcf21f411897be1accfed85afd35c2
nong508 <86358498+nong508@users.noreply.github.com>
2022-05-06 09:14:29 +0900

    create header

---
+++ 3week/HJ/Queue/linkeddequeue.h
@@ -0,0 +1,36 @@
+#ifndef _LINKED_DEQUE_
+#define _LINKED_DEQUE_
+​ typedef struct DequeNodeType
+{
+	char data;
+	struct DequeNodeType *pRLink;
+	struct DequeNodeType *pLLink;
+} DequeNode;
+​ typedef struct LinkedDequeType
+{
+	int currentElementCount; // 현재 원소의 개수
+	DequeNode *pFrontNode;	 // Front 노드의 포인터
+	DequeNode *pRearNode;	 // Rear 노드의 포인터
+} LinkedDeque;
+​
+	LinkedDeque *
+	createLinkedDeque();
+int insertFrontLD(LinkedDeque *pDeque, DequeNode element);
+int insertRearLD(LinkedDeque *pDeque, DequeNode element);
+DequeNode *deleteFrontLD(LinkedDeque *pDeque);
+DequeNode *deleteRearLD(LinkedDeque *pDeque);
+DequeNode *peekFrontLD(LinkedDeque *pDeque);
+DequeNode *peekRearLD(LinkedDeque *pDeque);
+void deleteLinkedDeque(LinkedDeque *pDeque);
+int isLinkedDequeFull(LinkedDeque *pDeque);
+int isLinkedDequeEmpty(LinkedDeque *pDeque);
+​
+#endif
+	​
+#ifndef _COMMON_QUEUE_DEF_
+#define _COMMON_QUEUE_DEF_
+		​
+#define TRUE 1
+#define FALSE 0
+			​
+#endif
