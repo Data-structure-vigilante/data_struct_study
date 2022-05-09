fe778dc460515562dce529496531470bad94a5d8
hekang42 <hekang@student.42seoul.kr>
2022-05-09 11:48:30 +0900

    modify simdef.c

--- 3week/HJ/CustomerSimulation/simdef.c
+++ 3week/HJ/CustomerSimulation/simdef.c
@@ -15,63 +15,78 @@
 // 	int endTime;
 // } SimCustomer;
 
-void insertCustomer(int arrivalTime, int processTime, LinkedDeque *pQueue)
-{
-	SimCustomer customer;
-	DequeNode node;
+void insertCustomer(int arrivalTime, int processTime, LinkedDeque *pQueue) {
+    SimCustomer customer;
+    DequeNode node;
 
-	customer.arrivalTime = arrivalTime;
-	customer.serviceTime = processTime;
-	customer.startTime = 0;
-	customer.endTime = 0;
-	node.data = customer;
-	insertRearLD(pQueue, node);
-	return;
+    customer.status = arrival;
+    customer.arrivalTime = arrivalTime;
+    customer.serviceTime = processTime;
+    customer.startTime = 0;
+    customer.endTime = 0;
+    node.data = customer;
+    insertRearLD(pQueue, node);
+    return;
 }
-void processArrival(int currentTime, LinkedDeque *pArrivalQueue, LinkedDeque *pWaitQueue)
-{
-	if (currentTime % 2 == 1)
-		return;
-	DequeNode *tmp = deleteFrontLD(pArrivalQueue);
-	insertRearLD(pWaitQueue, *tmp);
-	free(tmp);
-	return;
+void processArrival(int currentTime, LinkedDeque *pArrivalQueue,
+                    LinkedDeque *pWaitQueue) {
+    if (isLinkedDequeEmpty(pArrivalQueue))
+        return;
+    DequeNode *tmp = deleteFrontLD(pArrivalQueue);
+    insertRearLD(pWaitQueue, *tmp);
+    free(tmp);
+    return;
 }
-DequeNode *processServiceNodeStart(int currentTime, LinkedDeque *pWaitQueue);
+DequeNode *processServiceNodeStart(int currentTime, LinkedDeque *pWaitQueue) {
+    DequeNode *node;
+
+    node = peekFrontLD(pWaitQueue);
+    if (node->data.status == arrival) {
+        node->data.status = start;
+        node->data.startTime = currentTime;
+    } else if (currentTime - node->data.startTime == node->data.serviceTime) {
+        node = deleteFrontLD(pWaitQueue);
+        node->data.status = end;
+        return node;
+    }
+    return NULL;
+}
 DequeNode *processServiceNodeEnd(int currentTime, DequeNode *pServiceNode,
-								 int *pServiceUserCount, int *pTotalWaitTime);
+                                 int *pServiceUserCount, int *pTotalWaitTime) {
+    pTotalWaitTime += currentTime - (pServiceNode->data.arrivalTime +
+                                     pServiceNode->data.serviceTime);
+    pServiceUserCount += 1;
+    return pServiceNode;
+}
 void printSimCustomer(int currentTime, SimCustomer customer);
 void printWaitQueueStatus(int currentTime, LinkedDeque *pWaitQueue);
-void printReport(LinkedDeque *pWaitQueue, int serviceUserCount, int totalWaitTime);
+void printReport(LinkedDeque *pWaitQueue, int serviceUserCount,
+                 int totalWaitTime);
 
 // {3,2,1,1,3}
-int main(int ac, char **av)
-{
-	LinkedDeque *pArrivalQueue;
-	LinkedDeque *pWaitQueue;
-	int t = 0;
-	int i = 1;
+int main(int ac, char **av) {
+    LinkedDeque *pArrivalQueue;
+    LinkedDeque *pWaitQueue;
+    DequeNode *node;
+    int t = 0;
+    int i = 1;
+    int serviceUserCount = 0;
+    int totalWaitTime = 0;
 
-	pArrivalQueue = createLinkedDeque();
-	pWaitQueue = createLinkedDeque();
-	while (i < ac)
-	{
-		DequeNode node;
-		insertCustomer((i - 1) * 2, *av[i] - '0', pArrivalQueue);
-		++i;
-	}
-	// while (1)
-	// {
-	// 	processArrival(t, pArrivalQueue, pWaitQueue);
+    pArrivalQueue = createLinkedDeque();
+    pWaitQueue = createLinkedDeque();
+    while (1) {
+        if (t % 2 == 0 && i < ac)
+            insertCustomer(t, *av[i++] - '0', pArrivalQueue);
+        processArrival(t, pArrivalQueue, pWaitQueue);
 
-	// 	++t;
-	// }
+        node = processServiceNodeStart(t, pWaitQueue);
+        if (node)
+            free(processServiceNodeEnd(t, node, &serviceUserCount,
+                                       &totalWaitTime));
+        ++t;
+    }
+    printf("wait Time: %d\n", totalWaitTime);
 
-	printf("%d\n", deleteFrontLD(pArrivalQueue)->data.serviceTime);
-	printf("%d\n", deleteFrontLD(pArrivalQueue)->data.serviceTime);
-	printf("%d\n", deleteFrontLD(pArrivalQueue)->data.serviceTime);
-	printf("%d\n", deleteFrontLD(pArrivalQueue)->data.serviceTime);
-	printf("%d\n", deleteFrontLD(pArrivalQueue)->data.serviceTime);
-
-	return (0);
+    return (0);
 }
