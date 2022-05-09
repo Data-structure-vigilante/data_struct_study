56f6f06a673e9cdcdaa89e2126af2bf21323fc70
Jack-R-lantern <tjdfkr2421@gmail.com>
2022-05-09 10:39:25 +0900

    modify simulation

---
+++ 3week/SK/Simulation/simdef.h
@@ -0,0 +1,18 @@
+#ifndef __SIMDEF_H__
+#define __SIMDEF_H__
+
+typedef enum SimStatusType { 
+	ARRIVAL, 
+	START, 
+	END } SimStatus;
+
+typedef struct SimCustomerType
+{
+	SimStatus status;
+	int arrivalTime;
+	int serviceTime;
+	int startTime;
+	int endTime;
+} SimCustomer;
+
+#endif
