2b0cd3e9263616683ffe2e985b840a27c7505f6a
nong508 <86358498+nong508@users.noreply.github.com>
2022-06-02 10:30:52 +0900

    arraygraph by jinypark

--- 5week/SJK/arraygraph.c
+++ 5week/SJK/arraygraph.c
@@ -1,12 +1,12 @@
 #include "arraygraph.h"
 #include <stdlib.h>
 
-ArrayGraph*	createArrayGraph(int maxVertexCount)
+ArrayGraph *createArrayGraph(int maxVertexCount)
 {
-	ArrayGraph	*arrGraph;
-	int			**ppAdjedge;
-	int			*pVertex;
-	int			i;
+	ArrayGraph *arrGraph;
+	int **ppAdjedge;
+	int *pVertex;
+	int i;
 
 	arrGraph = (ArrayGraph *)calloc(1, sizeof(ArrayGraph));
 	ppAdjedge = (int **)calloc(maxVertexCount, sizeof(int *));
@@ -25,7 +25,80 @@ ArrayGraph*	createArrayGraph(int maxVertexCount)
 	return (arrGraph);
 }
 
-ArrayGraph* createArrayDirectedGraph(int maxVertexCount)
+ArrayGraph *createArrayDirectedGraph(int maxVertexCount)
 {
+	ArrayGraph *arrGraph;
 
+	arrGraph = createArrayGraph(maxVertexCount);
+	arrGraph->graphType = GRAPH_DIRECTED;
+	return (arrGraph);
 }
+
+void deleteArrayGraph(ArrayGraph *pGraph);
+
+int isEmptyAG(ArrayGraph *pGraph)
+{
+	return (pGraph->currentVertexCount == 0);
+}
+
+int checkVertexValid(ArrayGraph *pGraph, int vertexID)
+{
+	if (pGraph == NULL)
+		return (FALSE);
+	if (vertexID >= pGraph->maxVertexCount || vertexID < 0)
+		return (FALSE);
+	return (TRUE);
+}
+
+int addVertexAG(ArrayGraph *pGraph, int vertexID)
+{
+	if (pGraph == NULL)
+		return (FAIL);
+	if (checkVertexValid(pGraph, vertexID) == FALSE || pGraph->pVertex[vertexID] == USED)
+		return (FAIL);
+	pGraph->pVertex[vertexID] = USED;
+	pGraph->currentVertexCount++;
+
+	return (SUCCESS);
+}
+
+int addEdgewithWeightAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID, int weight)
+{
+	if (pGraph == NULL)
+		return (FAIL);
+	if (checkVertexValid(pGraph, fromVertexID) == FALSE || pGraph->pVertex[fromVertexID] == NOT_USED)
+		return (FAIL);
+	if (checkVertexValid(pGraph, toVertexID) == FALSE || pGraph->pVertex[toVertexID] == NOT_USED)
+		return (FAIL);
+	pGraph->ppAdjEdge[fromVertexID][toVertexID] = weight;
+	if (pGraph->graphType == GRAPH_UNDIRECTED)
+		pGraph->ppAdjEdge[toVertexID][fromVertexID] = weight;
+	return (SUCCESS);
+}
+
+int addEdgeAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID)
+{
+	return (addEdgewithWeightAG(pGraph, fromVertexID, toVertexID, 1));
+}
+
+int removeVertexAG(ArrayGraph *pGraph, int vertexID)
+{
+	if (pGraph == NULL || checkVertexValid(pGraph, vertexID) == FALSE || pGraph->pVertex[vertexID] == NOT_USED)
+		return (FAIL);
+	int i;
+
+	pGraph->pVertex[vertexID] == NOT_USED;
+	i = 0;
+	while (i < pGraph->maxVertexCount)
+	{
+		pGraph->ppAdjEdge[i][vertexID] = 0;
+		pGraph->ppAdjEdge[vertexID][i] = 0;
+		i++;
+	}
+	pGraph->currentVertexCount--;
+	return (SUCCESS);
+}
+
+int removeEdgeAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID);
+
+void displayArrayGraph(ArrayGraph *pGraph);
--- 5week/SJK/arraygraph.h
+++ 5week/SJK/arraygraph.h
@@ -5,43 +5,41 @@ typedef struct ArrayGraphType
 {
 	int maxVertexCount;
 	int currentVertexCount;
-	int graphType;			
-	int **ppAdjEdge;		
-	int *pVertex;			
+	int graphType;
+	int **ppAdjEdge;
+	int *pVertex;
 } ArrayGraph;
 
+ArrayGraph *createArrayGraph(int maxVertexCount);
+ArrayGraph *createArrayDirectedGraph(int maxVertexCount);
 
-ArrayGraph* createArrayGraph(int maxVertexCount);
-ArrayGraph* createArrayDirectedGraph(int maxVertexCount);
+void deleteArrayGraph(ArrayGraph *pGraph);
 
+int isEmptyAG(ArrayGraph *pGraph);
+int addVertexAG(ArrayGraph *pGraph, int vertexID);
+int addEdgeAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID);
+int addEdgewithWeightAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID, int weight);
 
-void deleteArrayGraph(ArrayGraph* pGraph);
+int checkVertexValid(ArrayGraph *pGraph, int vertexID);
+int removeVertexAG(ArrayGraph *pGraph, int vertexID);
 
-int isEmptyAG(ArrayGraph* pGraph)
-int addVertexAG(ArrayGraph* pGraph, int vertexID);
-int addEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID);
-int addEdgewithWeightAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID, int weight);
+int removeEdgeAG(ArrayGraph *pGraph, int fromVertexID, int toVertexID);
 
-int checkVertexValid(ArrayGraph* pGraph, int vertexID);
-int removeVertexAG(ArrayGraph* pGraph, int vertexID);
-
-int removeEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID);
-
-void displayArrayGraph(ArrayGraph* pGraph);
+void displayArrayGraph(ArrayGraph *pGraph);
 #endif
 
 #ifndef _COMMON_GRAPH_DEF_
 #define _COMMON_GRAPH_DEF_
 
-#define USED				1
-#define NOT_USED			0
+#define USED 1
+#define NOT_USED 0
 
-#define SUCCESS				1
-#define FAIL				0
+#define SUCCESS 1
+#define FAIL 0
 
-#define TRUE				1
-#define FALSE				0
+#define TRUE 1
+#define FALSE 0
 
-#define GRAPH_UNDIRECTED	1
-#define GRAPH_DIRECTED		2
+#define GRAPH_UNDIRECTED 1
+#define GRAPH_DIRECTED 2
 #endif
