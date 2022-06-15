33dafe6c15f62fe05e30e3ed608838cd3e6ca21c
Elineely <sooleeny@naver.com>
2022-06-15 11:15:29 +0900

    end until sort_info & write down some draft

--- 5week/SJK/kruskal/kruskal.c
+++ 5week/SJK/kruskal/kruskal.c
@@ -1,6 +1,6 @@
 #include "kruskal.h"
 
-void set_init(int *parent, int *num, int n)
+void set_init(int *parent, int *num, int n, edge_weight *info)
 {
 	int		i;
 
@@ -11,22 +11,28 @@ void set_init(int *parent, int *num, int n)
 		num[i] = 1;
 		++i;
 	}
+	i = 0;
+	while(i <= 1000)
+	{
+		info->start = -1;
+		info->end = -1;
+		info->weight = 0;
+	}
+	i = 0;
+	while(i < n - 1)
+	{
+
+	}
 }
 
-void kruskal(ArrayGraph *graph)
+void get_info(edge_weight *info, ArrayGraph *graph)
 {
-	int		*parent;
-	int 	*num;
 	int		a;
 	int		b;
-	edge_weight *info;
+	int		count;
 
 	a = 0;
-	parent = (int *)calloc(graph->maxVertexCount, sizeof(int));
-	num = (int *)calloc(graph->maxVertexCount, sizeof(int));
-	set_init(parent, num, graph->maxVertexCount);
-
-
+	count = 0;
 	while (a < graph->maxVertexCount)
 	{
 		b = a + 1;
@@ -37,12 +43,64 @@ void kruskal(ArrayGraph *graph)
 				info->start = a;
 				info->end = b;
 				info->weight = graph->ppAdjEdge[a][b];
+				++count;
 			}
 			++b;
 		}
 		++a;
 	}
+}
 
+void sort_info(edge_weight *info)
+{
+	int	temp;
+
+	for (int i = 0; i < info->edge_count - 1; i++)
+    {
+        for (int j = 0; j < info->edge_count - 1 - i; j++)
+        {
+            if (info[j].weight > info[j + 1].weight)
+            {
+                temp        = info[j].weight;
+                info[j].weight     = info[j + 1].weight;
+                info[j + 1].weight = temp;
+            }
+        }
+    }
+}
+
+void kruskal(ArrayGraph *graph)
+{
+	int		*parent;
+	int 	*num; //++++++++
+	int		*spanning_edge;
+	edge_weight *info;
+	int		a;
+	int		b;
+
+	parent = (int *)calloc(graph->maxVertexCount, sizeof(int));
+	num = (int *)calloc(graph->maxVertexCount, sizeof(int));
+	info = (edge_weight *)calloc(1000, sizeof(edge_weight));
+	spanning_edge = (int *)calloc(graph->maxVertexCount - 1, sizeof(int));
+	set_init(parent, num, graph->maxVertexCount, info, spanning_edge);
+	get_info(info, graph); //간선과 가중치 정보 배열
+	sort_info(info); //간선 오름차순(가중치)정렬
+	
+	while(b < n - 1 && a < info->edge_count)
+	{
+		if ( ! (is_cycle(info[a]))) //->start end 부모(루트)가 같은지 확인
+		{
+			set_union(info[a]);
+			spanning_edge[b] = info[a];
+		}
+		a++;
+	}
+	if (b != n - 1)
+	{
+		printf("실패!");
+		return;
+	}
+	~~~성공시 반환
 }
 
 int main()
--- 5week/SJK/kruskal/kruskal.h
+++ 5week/SJK/kruskal/kruskal.h
@@ -5,12 +5,13 @@ typedef struct edge_weight_type
 	int		start;
 	int		end;
 	int		weight;
+	int		edge_count;
 }				edge_weight;
 
 int set_find(int vertex); // 부모찾기
 
-void set_union(int s1, int s2); //집합 합치기
+void set_union(edge_weight info); //집합 합치기
 
 void kruskal(ArrayGraph *graph);
 
-void set_init(int *parent, int *num, int n);
+void set_init(int *parent, int *num, int n, edge_weight *info);
